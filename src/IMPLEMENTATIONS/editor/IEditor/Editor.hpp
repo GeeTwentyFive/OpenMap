010f3beb4d7a29a0c7cfc0a00feba9efdb9671cd
Gee <alickoamarko@gmail.com>
2025-07-17 08:31:53 +0200

    .

--- src/IMPLEMENTATIONS/editor/IEditor/Editor.hpp
+++ src/IMPLEMENTATIONS/editor/IEditor/Editor.hpp
@@ -57,6 +57,7 @@ public:
                                 input.UnlockCursor();
                         }
 
+
                         drawer.BeginDrawing();
                         {
 
--- src/INTERFACES/core/ILua.hpp
+++ src/INTERFACES/core/ILua.hpp
@@ -1,6 +1,16 @@
 #ifndef _OPENMAP_ILUA_HPP
 #define _OPENMAP_ILUA_HPP
 
+// LIBS:
+// - base
+// - package
+// - coroutine
+// - string
+// - os
+// - math
+// - table
+// - io
+
 // TODO: https://sol2.readthedocs.io/en/latest/tutorial/all-the-things.html
         // ^ + create & document Lua API
 
